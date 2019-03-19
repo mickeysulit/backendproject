8c4230823aea7ed63d0408f3b7acef86a1632a8e
Michey Sulit <mikaelaaasulit@gmail.com>
2019-03-19 14:32:14 +0800

    simulate logging in and logging out

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 <?php
 require 'mysql.php';
 
+echo session_status();
+//echo PHP_SESSION_NONE;
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+    echo session_status();
+    if($_SESSION['username']==NULL){
+    //haven't log in
+        echo "You haven't log in";
+        $_SESSION['username'] = 'mickey';
+    }else{
+    //Logged in
+        echo "Successfully log in!";
+        $_SESSION['username'] = NULL;
+    }
+} else {
+    echo "In Session";
+}
+
+
 //phpinfo();
 try {
         // use this command to connect to the database
